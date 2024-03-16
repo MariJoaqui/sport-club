e94fb1311245c426a510c5f5307be652f0294d19
Maria Joaquina <84864759+MariJoaqui@users.noreply.github.com>
2024-03-16 16:30:44 -0400

    Slider agregado.

---
+++ script/slider.js
@@ -0,0 +1,15 @@
+var animation = { duration: 25000, easing: (t) => t }
+  var slider = new KeenSlider("#slider", {
+    loop: true,
+    renderMode: "performance",
+    drag: false,
+    created(s) {
+      s.moveToIdx(5, true, animation)
+    },
+    updated(s) {
+      s.moveToIdx(s.track.details.abs + 5, true, animation)
+    },
+    animationEnded(s) {
+      s.moveToIdx(s.track.details.abs + 5, true, animation)
+    },
+});
